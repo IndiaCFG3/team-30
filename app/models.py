3ae61e4b6b2ee3352a31c0f558e5053c1bba3602
vedantparanjape <22630228+VedantParanjape@users.noreply.github.com>
2020-08-16 17:23:27 +0530

    added sql model

--- app/models.py
+++ app/models.py
@@ -5,14 +5,13 @@ from hashlib import md5
 from datetime import datetime
 from app import data
 
-
-
 class teacher(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     role = db.Column(db.String(1), default='t')
+    lectures = db.relationship('lectureHistory')
 
     def __repr__(self):
         return '<teacher {}>'.format(self.name)
@@ -50,16 +49,37 @@ class studentHistory(db.Model):
     name = db.Column(db.String(64), index=True, unique=True, nullable=False)
     phoneno = db.Column(db.Integer)
     message_option = db.Column(db.String(1), default='s')
-    marks = db.Column(db.Integer)
+    marks = db.relationship('scores')
 
     def __repr__(self):
-        return '<teacherHistory {} {} {} {}>'.format(self.id, self.name, self.phoneno, self.marks)
+        return '<studentHistory {} {} {} {}>'.format(self.id, self.name, self.phoneno)
+
+class lectureHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    link = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    transcript = db.Column(db.String(100000), index=True)
+    teacher_id = db.Column(db.Integer, ForeignKey('teacher.id'))
+
+    def __repr__(self):
+        return '<lectureHistory {} {} {} {}>'.format(self.id)
 
 class assignments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(10000))
-    answer = db.Column(db.String(10))
-        
+    option1 = db.Column(db.String(100))
+    option2 = db.Column(db.String(100))
+    option3 = db.Column(db.String(100))
+    option4 = db.Column(db.String(100))
+    answer = db.Column(db.Integer)
+
+    scores = db.Column(db.Integer, ForeignKey('scores.id'))
+
+class scores(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    marks = db.Column(db.Integer)
+    student_id = db.Column(db.Integer, ForeignKey('studentHistory.id'))
+    assignments_id = db.relationship('assignments')
+
 @login.user_loader
 def load_teacher(id):
     print("userloader called", data.check_type())
